cbf0f51a65ecd959ceacd039779a9e5ce32952f8
Nikolay Kachanov <bulian1311@gmail.com>
2018-11-01 10:45:20 +0300

    Procfile added

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   app: process.env.APP || 'dev',
   port: process.env.PORT || '4000',
   mongo_db:
-    process.env.MONGO_DB ||
+    //process.env.MONGO_DB ||
     'mongodb://nikolay:nikolay1311@ds016108.mlab.com:16108/magmer'
 };
