32526b1e72078954622f7db82d91541b858c2af3
Nikolay Kachanov <bulian1311@gmail.com>
2018-11-06 14:36:04 +0300

    coa/cors midleware added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const Koa = require('koa');
 const json = require('koa-json');
 const bodyparser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 const mongoose = require('mongoose');
+
 const config = require('./config');
 const router = require('./router');
 
@@ -22,6 +24,7 @@ const app = new Koa();
 
 app.use(json());
 app.use(bodyparser());
+app.use(cors());
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(config.port, () => console.log(`App listen on port ${config.port}`));
