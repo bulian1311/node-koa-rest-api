f4b2e6a1691ae7a7bd8e27ab3138229b214a51d5
Nikolay Kachanov <bulian1311@gmail.com>
2018-11-21 16:10:33 +0300

    add jwt protection for router

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ module.exports = {
   port: process.env.PORT || '4000',
   mongo_db:
     //process.env.MONGO_DB ||
-    'mongodb://nikolay:nikolay1311@ds016108.mlab.com:16108/magmer'
+    'mongodb://nikolay:nikolay1311@ds016108.mlab.com:16108/magmer',
+  secret: 'MagMerTheBest'
 };
--- src/router.js
+++ src/router.js
@@ -1,13 +1,20 @@
 const Router = require('koa-router');
 const homeController = require('./controllers/home');
 const productController = require('./controllers/product');
+const jwt = require('koa-jwt');
+
+const config = require('./config');
 
 const router = new Router();
 
+// Public routes
 router.get('/', homeController.home);
-
 router.get('/product', productController.getAll);
 router.get('/product/:id', productController.getOne);
+
+router.use(jwt({ secret: config.secret }));
+
+// Protected routes
 router.post('/product', productController.create);
 router.put('/product/:id', productController.update);
 router.delete('/product/:id', productController.delete);
