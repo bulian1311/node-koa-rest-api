a3e1570bc6193b7386288101eaff082420ffb7e6
Nikolay Kachanov <bulian1311@gmail.com>
2018-11-26 10:49:43 +0300

    test database url changed

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   port: process.env.PORT || '4000',
   mongo_db:
     //process.env.MONGO_DB ||
-    'mongodb://nikolay:nikolay1311@ds016108.mlab.com:16108/magmer',
+    'mongodb://bulian1311:bulian1311@ds026018.mlab.com:26018/magmer-test',
   secret: 'MagMerTheBest'
 };
